353c0fb53e95ab27eadebf986aba4eb8d8e41278
Obadiah Korir <backendcoders@gmail.com>
2025-11-30 14:10:43 +0300

    Add project files.

---
+++ DatabaseMapping/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagementAPI.DatabaseMapping
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
